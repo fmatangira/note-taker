b90456d120b8dfe497637a78941e5da595d896bf
Farai Matangira <fmatangira@gmail.com>
2020-01-10 23:17:11 -0600

    finished setting up the server.

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 8081;
+const path = require('path');
+
+// const html = fs.readFile('../index.html',(err) => {
+//     if (err) throw err;
+// });
+
+app.get('/',function(req,res){
+    res.sendfile(path.join(__dirname,'../../index.html'))
+})
+
+app.get('/notes',function(req,res){
+    res.sendfile(path.join(__dirname,'../../notes.html'))
+})
+
+app.listen(PORT,function() {
+    console.log(`Server is listening on port ${PORT}`);  
+})
